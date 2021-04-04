c77ae7b745de23695a709789453a2114617ab3da
Israel Moraes <israelfmoraes@outlook.com>
2021-04-04 01:16:22 -0300

    Update share, rights and contact contents. Others SCSS

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210403.2358';
+var cacheName = 'chirpy-20210404.0115';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
