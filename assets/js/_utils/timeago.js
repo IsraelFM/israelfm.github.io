ee7c58854bb74b33ad5e8332cf18d8ed3d88ff54
IsraelFM <israelfmoraes@outlook.com>
2021-04-09 23:30:22 -0300

    Translate all texts

--- assets/js/_utils/timeago.js
+++ assets/js/_utils/timeago.js
@@ -51,7 +51,7 @@ $(function () {
             return minute + " minute" + (minute > 1 ? "s" : "") + " ago";
         }
 
-        return "just now";
+        return "agora mesmo";
     }
 
     function updateTimeago() {
